a0887d7b7918c95d1b64a892ffb9baa8a33a6a75
tinyweet <tinyweet@gmail.com>
2022-05-11 17:12:43 +0800

    fix: mulit rule-group extract

--- README.md
+++ README.md
@@ -9,8 +9,11 @@ mvn clean package
 ```
 ## execute
 ```shell
- java -jar target/penny-practice3-1.0-SNAPSHOT.jar "这个老板不怎么骗人，但是一次上门体验的经历还是让我非常后悔。" "这块蛋糕没有异味。"
+java -jar target/penny-practice3-1.0-SNAPSHOT.jar "这个老板不怎么骗人，但是一次上门体验的经历还是让我非常后悔。" "这块蛋糕没有异味。" "我非常地放心这一个没有出过问题的电吹风"
 ```
 > 不.骗人 -1;非常.后悔 5
 > 
 > 没.异味 -1
+> 
+> 没.问题 -1;非常.放心 -1.5
+
--- src/main/java/info/tinywit/FeatureExtractor.java
+++ src/main/java/info/tinywit/FeatureExtractor.java
@@ -6,28 +6,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class FeatureExtractor {
-    public static Map<String, String> initRuleList() {
-        LinkedHashMap<String, String> ruleList = new LinkedHashMap<>();
+    public static List<String> initRuleList() {
+        List<String> ruleList = new LinkedList();
         BufferedReader bufferedReader = null;
         try {
             bufferedReader = new BufferedReader(new InputStreamReader(FeatureExtractor.class.getResourceAsStream("/rule.txt"), StandardCharsets.UTF_8));
-
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 String[] rl = StringUtils.split(line, ":");
-                if (rl == null) {
-                    continue;
-                }
-                for (int i = 0; i < rl.length; i++) {
-                    String[] r = StringUtils.split(rl[i], " ");
-                    if (r.length > 1) {
-                        ruleList.put(StringUtils.trim(r[0]), StringUtils.trim(r[1]));
-                    }
+                if (rl != null) {
+                    ruleList.addAll(Arrays.asList(rl));
+                    ruleList.add("");
                 }
             }
         } catch (Exception e) {
@@ -40,55 +35,61 @@ public class FeatureExtractor {
                 }
             }
         }
-
         return ruleList;
     }
 
     public static String extract(String... textList) {
-        Map<String, String> ruleList = initRuleList();
+        List<String> ruleList = initRuleList();
         StringBuilder sb = new StringBuilder();
         for (String text : textList) {
-            if (sb.length() != 0) {
-                sb.append(System.lineSeparator());
-            }
-            sb.append(extract(text, ruleList));
+            append(sb, extract(text, ruleList), System.lineSeparator());
         }
-
         return sb.toString();
     }
 
-    public static String extract(String text, Map<String, String> ruleList) {
+    public static String extract(String text, List<String> ruleList) {
         String[] sentences = StringUtils.split(text, ",，");
         StringBuilder sb = new StringBuilder();
         if (sentences != null) {
             for (int i = 0; i < sentences.length; i++) {
                 String tmp = extractFromSentence(sentences[i], ruleList);
-                if (StringUtils.isBlank(tmp)) {
-                    continue;
+                if (!StringUtils.isBlank(tmp)) {
+                    append(sb, tmp, ";");
                 }
-                if (sb.length() != 0) {
-                    sb.append(";");
-                }
-                sb.append(tmp);
             }
         }
-
         return sb.toString();
     }
 
-    private static String extractFromSentence(String sentence, Map<String, String> ruleList) {
-        String ret = "";
-        for (Map.Entry<String, String> entry : ruleList.entrySet()) {
-            if (Pattern.matches(pattern(entry.getKey()), sentence)) {
-                ret = entry.getKey() + " " + entry.getValue();
-                break;
+    private static String extractFromSentence(String sentence, List<String> ruleList) {
+        boolean isNextGroup = true;
+        StringBuilder sb = new StringBuilder();
+        for (String rule : ruleList) {
+            if (StringUtils.isBlank(rule)) {
+                isNextGroup = true;
+                continue;
+            }
+            if (!isNextGroup) {
+                continue;
+            }
+
+            String[] rp = StringUtils.split(rule, " ", 2);
+            if (Pattern.matches(pattern(rp[0]), sentence)) {
+                append(sb, rp[0] + " " + rp[1], ";");
+                isNextGroup = false;
             }
         }
-
-        return ret;
+        return sb.toString();
     }
 
     private static String pattern(String rule) {
         return StringUtils.replace("." + rule + ".", ".", ".*");
+    }
+
+    private static StringBuilder append(StringBuilder sb, String str, String sep) {
+        if (sb.length() != 0) {
+            sb.append(sep);
+        }
+        return sb.append(str);
     }
 }
